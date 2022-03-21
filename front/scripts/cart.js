2412eb7e171cd7606bbfffab5d260c90a183d073
Kirko87 <francescoragone87@gmail.com>
2022-03-21 11:28:14 +0100

    21-03-2022

--- front/scripts/cart.js
+++ front/scripts/cart.js
@@ -1,9 +1,6 @@
 async function main(){  
     const response=await ('http://192.168.1.16:5500/front/html/cart.html')
    
-
-    console.log('rmknke');
-     
     var datiOggettiCestino = localStorage.getItem("carrello");
     console.log(datiOggettiCestino);
     const carrelloProdottiJs = JSON.parse(datiOggettiCestino)
@@ -14,25 +11,35 @@ async function main(){
     const item=await risposta.json();
     
     const contenitorePrimo = document.querySelector("#infoCarrello").content.cloneNode(true)
-    contenitorePrimo.querySelector("#cart__item").dataset.id=oggettiCarrello.id
-    contenitorePrimo.querySelector("#cart__item").dataset.color=oggettiCarrello.colore
+    contenitorePrimo.querySelector(".cart__item").dataset.id=oggettiCarrello.id
+    contenitorePrimo.querySelector(".cart__item").dataset.color=oggettiCarrello.colore
     contenitorePrimo.querySelector(".productImage").src=item.imageUrl
     contenitorePrimo.querySelector(".cart__product__name").innerText=item.name
     contenitorePrimo.querySelector(".cart__product__color").innerText = oggettiCarrello.colore
     contenitorePrimo.querySelector(".cart__product__price").innerText = item.price +"€"
     contenitorePrimo.querySelector(".itemQuantity").value=oggettiCarrello.quantita
-    
-    
- 
-    const gatto = document.getElementById("cart__items")
-        gatto.appendChild(contenitorePrimo)
- 
 
-    
+    const baliseRec = document.getElementById("cart__items")
+        baliseRec.appendChild(contenitorePrimo)
 
+        const modificaQ = contenitorePrimo.querySelector(".itemQuantity")
+      
+        modificaQ.addEventListener("change", function (){
+                   const quantity=modificaQ.value
+                   console.log("peppe");
+                   })
     }
 
     
+ 
+                  
+ 
+
+       
+
+    
+
+
 }
 
 main (
