4bad0203146954e617451ba83666443dc3283e64
Kirko87 <francescoragone87@gmail.com>
2022-03-23 21:35:20 +0100

    23-03-2022

--- front/scripts/cart.js
+++ front/scripts/cart.js
@@ -2,17 +2,19 @@ async function main() {
 
 
     var datiOggettiCestino = localStorage.getItem("carrello");
-    console.log(datiOggettiCestino);
+    //console.log(datiOggettiCestino);
     const carrelloProdottiJs = JSON.parse(datiOggettiCestino)
-    console.log(carrelloProdottiJs);
+    //console.log(carrelloProdottiJs);
 
 
     const itemList = []
+
 
     for (index in carrelloProdottiJs) {
         const oggettiCarrello = carrelloProdottiJs[index]
         const risposta = await fetch('http://localhost:3000/api/products/' + oggettiCarrello.id)
         const item = await risposta.json();
+
         itemList.push(item)
         const contenitorePrimo = document.querySelector("#infoCarrello").content.cloneNode(true)
         contenitorePrimo.querySelector(".cart__item").dataset.id = oggettiCarrello.id
@@ -23,42 +25,64 @@ async function main() {
         contenitorePrimo.querySelector(".cart__product__price").innerText = item.price + "€"
         contenitorePrimo.querySelector(".itemQuantity").value = oggettiCarrello.quantita
 
+
         const modificaQ = contenitorePrimo.querySelector(".itemQuantity")
+
+        const deleteItemButton = contenitorePrimo.querySelector("#deleteItem")
+
+
+        //change quantity produits
+
 
         modificaQ.addEventListener("change", function () {
             const quantityMOD = parseInt(modificaQ.value)
-
             oggettiCarrello.quantita = quantityMOD
-
             let carrello = JSON.stringify(carrelloProdottiJs);
-
             localStorage.setItem("carrello", carrello);
-            console.log(carrello);
 
             calcolaTotale(carrelloProdottiJs, itemList)
 
         })
 
 
+        //delete products
+
+         deleteItemButton.addEventListener("click", function () {
+                 var re = carrelloProdottiJs.splice(index, 1);
+                 console.log(re);
+                 let carrello = JSON.stringify(carrelloProdottiJs);
+                 localStorage.setItem("carrello", carrello);
+
+                 calcolaTotale(carrelloProdottiJs, itemList);
+
+             })
+
+        //registra modifiche in balise globale <section id="cart__items">
         const baliseRec = document.getElementById("cart__items")
         baliseRec.appendChild(contenitorePrimo)
-
-
     }
 
+
     calcolaTotale(carrelloProdottiJs, itemList)
+
 }
 
-function calcolaTotale(cart, visualizza) {
+
+//calcolaTotale function 
+
+
+function calcolaTotale(cartProduits, listaElementi) {
     let totalQuantity = 0
     let totalPrice = 0
 
-    for (cartItems of cart) {
-        const item = visualizza.find(fff => fff._id === cartItems.id)
+
+    for (cartItems of cartProduits) {
+        const item = listaElementi.find(x => x._id === cartItems.id)
+
         totalQuantity += cartItems.quantita
-        console.log(totalQuantity);
+
         totalPrice += (item.price * cartItems.quantita)
-        console.log(totalPrice);
+
     }
     document.getElementById("totalQuantity").innerText = totalQuantity
     document.getElementById("totalPrice").innerText = totalPrice
--- front/scripts/product.js
+++ front/scripts/product.js
@@ -1,93 +1,91 @@
-async function main(){  
+async function main() {
     var queryString = window.location.search;
     let params = new URLSearchParams(queryString);
     let id = params.get("id");
-    const response=await fetch('http://localhost:3000/api/products/'+id)
-    const item=await response.json()
+    const response = await fetch('http://localhost:3000/api/products/' + id)
+    const item = await response.json()
 
- /* recupero prezzo, nome articolo e descrizione*/   
-    console.log(item)
+    /* recupero prezzo, nome articolo e descrizione*/
+    //console.log(item)
 
-    document.querySelector("#price").innerText=item.price;
-    document.querySelector("#title").innerText=item.name;
-    document.querySelector("#description").innerText=item.description;
+    document.querySelector("#price").innerText = item.price;
+    document.querySelector("#title").innerText = item.name;
+    document.querySelector("#description").innerText = item.description;
 
- /* recupero immagine prodotto*/
-   
-    const fotografia=document.createElement("img");
-    let baliseParent=document.getElementById("fotoCanape");
-    fotografia.src=item.imageUrl;
-    fotografia.alt=item.altTxt;
+    /* recupero immagine prodotto*/
+
+    const fotografia = document.createElement("img");
+    let baliseParent = document.getElementById("fotoCanape");
+    fotografia.src = item.imageUrl;
+    fotografia.alt = item.altTxt;
     baliseParent.appendChild(fotografia);
 
     /* rendere valore colori come opzione in select*/
 
-    for(let colori of item.colors){
-        const option= document.createElement("option")
-        option.innerText=colori
-        const selectColors=document.getElementById("colors")
+    for (let colori of item.colors) {
+        const option = document.createElement("option")
+        option.innerText = colori
+        const selectColors = document.getElementById("colors")
         selectColors.appendChild(option)
     }
 
-    /* errore browser*/ 
+    /* errore browser*/
     if (typeof browser === "undefined") {
         var browser = chrome;
     }
 
-     /*recupero dei valori in localStorage*/    
-      const pulsante = document.getElementById("addToCart")
-      pulsante.addEventListener("click",function(){
-      if(quantity.value <=0){
-          alert("seleziona una quantità superiore a 0")
-          return 
-      }
-    
-     /*     if (coloreSelezionato === ""){
-        alert("seleziona un colore")
-        return
+    /*recupero dei valori in localStorage*/
+    const pulsante = document.getElementById("addToCart")
+    pulsante.addEventListener("click", function () {
+        if (quantity.value <= 0) {
+            alert("seleziona una quantità superiore a 0")
+            return
         }
-    */
-      
-     /* recupero valori colore del select */
-      var colors=document.getElementById("colors")
-      var coloreSelezionato = colors.options[colors.selectedIndex].value;  
 
+        /*     if (coloreSelezionato === ""){
+           alert("seleziona un colore")
+           return
+           }
+       */
 
- 
-     //definisco valori Array
-     const cartItems = JSON.parse (localStorage.getItem("carrello")) || []
-     let cartItem = cartItems.find(function(_cartItem){
-      return item._id===_cartItem.id && coloreSelezionato===_cartItem.colore
-     })
-     if (cartItem){
-        cartItem.quantita+=parseInt(quantity.value);
+        /* recupero valori colore del select */
+        var colors = document.getElementById("colors")
+        var coloreSelezionato = colors.options[colors.selectedIndex].value;
 
-     }else{
-            cartItem = {   
+        //definisco valori Array
+        const cartItems = JSON.parse(localStorage.getItem("carrello")) || []
+        let cartItem = cartItems.find(function (_cartItem) {
+                return item._id === _cartItem.id && coloreSelezionato === _cartItem.colore;
+            })
+        if (cartItem) {
+            cartItem.quantita += parseInt(quantity.value);
 
-            id: item._id,
-            colore: coloreSelezionato,
-            quantita:parseInt(quantity.value)
+        } else {
+            cartItem = {
 
-         }
-         cartItems.push(cartItem)
-     }
-       
-    //trasforma oggetto JS in Json
+                id: item._id,
+                colore: coloreSelezionato,
+                quantita: parseInt(quantity.value)
 
-     let carrello = JSON.stringify(cartItems);
-     console.log(carrello);
+            }
+            cartItems.push(cartItem)
+        }
 
-     localStorage.setItem("carrello",carrello);
-     console.log(coloreSelezionato,item._id,quantity.value) //verifica valori inviati
-      }
-      )
+        //trasforma oggetto JS in Json
+
+        let carrello = JSON.stringify(cartItems);
+        // console.log(carrello);
+
+        localStorage.setItem("carrello", carrello);
+        //console.log(coloreSelezionato,item._id,quantity.value) //verifica valori inviati
+    }
+    )
 
 }
- 
+
 
 main()
 
-   
-    
+
+
 
