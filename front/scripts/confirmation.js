fd22496d9c54a1d3c9823d30d657b28c5a153506
Kirko87 <francescoragone87@gmail.com>
2022-03-31 15:58:50 +0200

    /

--- front/scripts/confirmation.js
+++ front/scripts/confirmation.js
@@ -1,14 +1,14 @@
 
-   var recupera = localStorage.getItem("idComanda")
-  
-    console.log('der');
-  
-    let spanId = document.getElementById("orderId")
-     spanId.innerHTML = "<br><br>" + recupera
+var recupera = localStorage.getItem("idComanda")
+
+//console.log(recupera);
+
+let spanId = document.getElementById("orderId")
+spanId.innerHTML = "<br><br>" + recupera
 
 
-   
 
-   
+
+
 
 
