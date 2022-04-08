80f271c24f1b34bab298da6e769c1cf6fcdddc04
Kirko87 <francescoragone87@gmail.com>
2022-04-08 18:38:35 +0200

    08-04-2022

--- front/scripts/confirmation.js
+++ front/scripts/confirmation.js
@@ -4,7 +4,7 @@ var queryString = window.location.search;
 let params = new URLSearchParams(queryString);
 
 let spanId = document.getElementById("orderId")
-spanId.innerHTML = "<br><br>" + params.get('idOrder')
+spanId.innerText = params.get('idOrder')
 
 localStorage.removeItem("carrello");
 
